60853689b77e3d44e4d221d8c6dcbc2333426aaa
JayeshMalviya5 <115400817+JayeshMalviya5@users.noreply.github.com>
2023-01-06 20:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 function palindrome(str){
     // get the total length of the words  
     const len = str.length;  
+	str.toLowerCase();
   
     // Use for loop to divide the words into 2 half  
     for (let i = 0; i < len / 2; i++) {  
