9e5b5aa27e04bfdbbdbfb41cf11f2118d90f08cb
JayeshMalviya5 <115400817+JayeshMalviya5@users.noreply.github.com>
2023-01-06 20:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+  let high = str.length-1;
+  let low = 0;
+  while(low<=high){
+     if(str[low]!=str[high]){
+        return false;
+     }
+     ++low;
+     --high;
+  }
+  return true;
 }
 module.exports = palindrome
